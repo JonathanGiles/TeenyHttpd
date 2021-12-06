c57301ec56d2f8028970baa0172d183a3ccb5a53
Jonathan Giles <jonathan@jonathangiles.net>
2021-12-07 10:37:38 +1300

    Updating dependencies and preparing for 1.0.3 release

---
+++ src/main/java/module-info.java
@@ -0,0 +1,5 @@
+module net.jonathangiles.tools.teenyhttpd {
+    exports net.jonathangiles.tools.teenyhttpd;
+    exports net.jonathangiles.tools.teenyhttpd.request;
+    exports net.jonathangiles.tools.teenyhttpd.response;
+}
